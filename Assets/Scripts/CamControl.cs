3d8cd3cfb3af24d6a0d6b08af5113cbeb1f70806
Godzzo <egodzzo@gmail.com>
2020-03-06 13:50:02 +0100

    CamControl added

--- Assets/Scripts/CamControl.cs
+++ Assets/Scripts/CamControl.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CamControl : MonoBehaviour
 {
+    [SerializeField] Transform _target;
+    [SerializeField] Vector3 _offset;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,8 @@ public class CamControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var camPos = _target.position + _offset;
         
+        transform.position = camPos;
     }
 }
