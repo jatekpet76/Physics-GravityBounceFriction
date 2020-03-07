245a596f352d5f7843127f1e0a6d392114946a55
Godzzo <egodzzo@gmail.com>
2020-03-07 06:20:28 +0100

    Prepare for Exam Bouncy Box

--- Assets/Scripts/MoveAroundControl.cs
+++ Assets/Scripts/MoveAroundControl.cs
@@ -2,19 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurnCamControl : MonoBehaviour
+public class MoveAroundControl : MonoBehaviour
 {
     [SerializeField] Transform _target;
-    
+    [SerializeField] Vector3 _velocity;
+    Rigidbody _rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _rigidbody.AddForce((_velocity * Time.deltaTime));
     }
 }
