f3507d22a4b04de82d6ff8a5427c353c142ef026
Godzzo <egodzzo@gmail.com>
2020-03-09 10:17:45 +0100

    Adding LateStart and Reset

--- Assets/Scripts/TryMoveAroundControl.cs
+++ Assets/Scripts/TryMoveAroundControl.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class TryMoveAroundControl : MonoBehaviour
 {
-    
     [SerializeField] Vector3 _velocity;
     Rigidbody _rigidbody;
 
     Vector3 _previousPosition = new Vector3(0, 0, 0);
     Vector3 _fixedPreviousPosition = new Vector3(0, 0, 0);
+    Vector3 _ballPosition = new Vector3(0, 0, 0);
 
     bool _isForceAdded = false;
 
@@ -17,17 +17,40 @@ public class TryMoveAroundControl : MonoBehaviour
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _ballPosition = transform.position;
+
+        StartCoroutine(LateStart());
+    }
+
+    IEnumerator LateStart() {
+        Debug.Log("Late Start!");
+        yield return new WaitForSeconds(1);
+
+        Debug.Log("Call AddForce");
+        AddForce();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        if (Input.GetKey(KeyCode.R)) {
+            Reset();
+        }
+
         Debug.Log(string.Format("Update - Name: {0}, Position: {1}, deltaTime: {2}, frameCount: {3}, change: {4}, time: {5}", 
             gameObject.name, transform.position, Time.deltaTime, Time.frameCount, GetPositionChanges(_previousPosition), Time.time));
 
         _previousPosition = transform.position;
 
-        AddForce();
+        // AddForce();
+    }
+
+    private void Reset() {
+        Debug.Log("Reset the Ball!");
+
+        _rigidbody.velocity = new Vector3(0f, 0f, 0f);
+        _rigidbody.angularVelocity = new Vector3(0f, 0f, 0f);
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+        transform.position = _ballPosition;
     }
 
     private void FixedUpdate() {
