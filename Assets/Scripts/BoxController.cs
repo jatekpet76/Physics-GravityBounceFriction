a7b2384896593529ef542b8ffdb7212cfe00812d
Godzzo <egodzzo@gmail.com>
2020-03-09 10:30:27 +0100

    Adding GamePad controlling

--- Assets/Scripts/BoxController.cs
+++ Assets/Scripts/BoxController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BoxController : MonoBehaviour
 {
     [SerializeField] float _step = 1;
+    [SerializeField] bool _gamepadEnabled = true;
 
     // Start is called before the first frame update
     void Start()
@@ -15,15 +16,22 @@ public class BoxController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var zPos = (Input.GetKey(KeyCode.J) ? _step: 0) * Time.deltaTime;
-        var zNeg = (Input.GetKey(KeyCode.L) ? -1*_step: 0) * Time.deltaTime;
-        var xPos = (Input.GetKey(KeyCode.I) ? _step: 0) * Time.deltaTime;
-        var xNeg = (Input.GetKey(KeyCode.K) ? -1*_step: 0) * Time.deltaTime;
-        var yPos = (Input.GetKey(KeyCode.U) ? _step: 0) * Time.deltaTime;
-        var yNeg = (Input.GetKey(KeyCode.O) ? -1*_step: 0) * Time.deltaTime;
+        
+
+        var zPos = ((Input.GetKey(KeyCode.J) ? _step: 0) * Time.deltaTime) + 
+            ((Input.GetKey(KeyCode.L) ? -1*_step: 0) * Time.deltaTime);
+        var xPos = ((Input.GetKey(KeyCode.I) ? _step: 0) * Time.deltaTime) +
+            ((Input.GetKey(KeyCode.K) ? -1*_step: 0) * Time.deltaTime);
+        var yPos = ((Input.GetKey(KeyCode.U) ? _step: 0) * Time.deltaTime) +
+            ((Input.GetKey(KeyCode.O) ? -1*_step: 0) * Time.deltaTime);
+
+        if (_gamepadEnabled) { 
+            xPos = Input.GetAxis("Vertical") *_step * Time.deltaTime;
+            zPos = Input.GetAxis("Horizontal") *_step * Time.deltaTime;
+        }
 
         var euler = transform.rotation.eulerAngles;
 
-        transform.rotation = Quaternion.Euler(euler.x + (xPos + xNeg), euler.y + (yPos + yNeg), euler.z + (zPos + zNeg));
+        transform.rotation = Quaternion.Euler(euler.x + xPos, euler.y + yPos, euler.z + zPos);
     }
 }
